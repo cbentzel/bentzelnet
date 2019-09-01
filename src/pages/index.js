77f1f88e51c2ce872cebc6ee2e1db562b236ed7f
Chris Bentzel <chris@bentzel.net>
2019-09-01 13:13:59 -0400

    Change top-level page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>chris.bentzel.net</h1>
+    <p>Trying out some small modifications.</p>
+    <p>Everything is awesome.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
